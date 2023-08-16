1e7711f12b804d755088d8314cb8efcb0d50cbfc
shubh58757 <132486633+shubh58757@users.noreply.github.com>
2023-08-16 22:04:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 pygame.init()
 width = 1000
 height =  500
-screen = pygame.display.set_mode((1000,500))
+screen = pygame.display.set_mode((width,height))
 FPS = 30
 fpsClock = pygame.time.Clock()
 
